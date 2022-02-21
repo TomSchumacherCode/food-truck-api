f640dd1cd351d2d0bc4edbc36e10307229278c6b
Mike Truax <mike@miketruax.com>
2022-02-21 14:25:56 -0600

    finishes gif reducer

--- src/redux/actions/gifs.actions.js
+++ src/redux/actions/gifs.actions.js
@@ -1,4 +1,20 @@
 export const ADD_FAVORITE = "Add Favorite";
 export const REMOVE_FAVORITE = "Remove Favorite";
 export const SET_SEARCH = "Set Search";
-export const CLEAR_GIFS = "CLEAR Gifs";
+export const CLEAR_GIFS = "Clear Gifs";
+
+export const addFavorite = (gif) => {
+  return { type: ADD_FAVORITE, gif };
+};
+
+export const removeFavorite = (id) => {
+  return { type: REMOVE_FAVORITE, id };
+};
+
+export const setSearch = (results) => {
+  return { type: SET_SEARCH, results };
+};
+
+export const clearGifs = () => {
+  return { type: CLEAR_GIFS };
+};
--- src/redux/reducers/gifs.reducer.js
+++ src/redux/reducers/gifs.reducer.js
@@ -1,4 +1,30 @@
+import {
+  ADD_FAVORITE,
+  REMOVE_FAVORITE,
+  CLEAR_GIFS,
+  SET_SEARCH,
+} from "../actions";
+
 const initialState = {
   search: [],
   favorites: [],
 };
+
+export default function gifsReducer(state, action) {
+  switch (action.type) {
+    case ADD_FAVORITE:
+      //! Keep search in tact, create a new array with the new gif added to it
+      return { ...state, favorites: [...state.favorites, action.gif] };
+    case REMOVE_FAVORITE:
+      return {
+        ...state,
+        favorites: state.favorites.filter((val) => val.id !== action.id),
+      };
+    case CLEAR_GIFS:
+      return { ...state, search: [], favorites: [] };
+    case SET_SEARCH:
+      return { ...state, search: action.results };
+    default:
+      return state;
+  }
+}
