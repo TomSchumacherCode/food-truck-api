d2aa3c563e6af257b5521f96affe7bd2dfe1f31a
Mike Truax <mike@miketruax.com>
2022-03-03 10:50:20 -0600

    adds todos for routes files

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,2 +1,9 @@
 //! /api/auth/login - POST
+
+//TODO Implement the login model function
+//TODO Check if there are the following keys on the request body (with values of appropriate lengths):
+//? username, password
 //! /api/auth/signup - PUT
+//TODO Check if there are the following keys on the request body  (with values of appropriate lengths):
+//? username, password
+//TODO Implement the signup model function
--- server/routes/favorites.routes.js
+++ server/routes/favorites.routes.js
@@ -1,3 +1,10 @@
 //! /api/favorites/:user_id - GET
+//TODO Implement the byUserID model function
+
 //! /api/favorites/add - PUT
+//TODO Check if there are the following keys on the request body (with values):
+//? user_id, gif_id, title, url
+//TODO Implement the add model function
+
 //! /api/favorites/remove/:id - DELETE
+//TODO Implement the remove model function
