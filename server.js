60ac5f50a882cdae179b4bd0ee7a0b36834ad03e
Thomas.Schumacher <Tommyschumacher@gmail.com>
2022-03-09 10:26:35 -0600

    cleans up file and adds CORS library fix

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const authRoutes = require("./server/routes/auth.routes");
-const favoritesRoutes = require("./server/routes/favorites.routes");
 const testRoutes = require("./server/routes/test.routes");
 
 
 app.use(express.json());
 app.use(express.static(__dirname + "/build"));
+app.use(cors({origin:true}));
 
 app.use("/api/users", authRoutes);
-app.use("/api/favorites", favoritesRoutes);
 app.use("/api/testing", testRoutes);
 
 app.get("*", (req, res) => {
--- server/routes/test.routes.js
+++ server/routes/test.routes.js
@@ -2,15 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/json", (req, res) => {
+    // res.header("Access-Control-Allow-Origin", "*")
+
     return res.send({
         data: { test: "SUCCESS" },
         success: true,
         error: null,
-      });
+    });
 });
 
 router.get("/html", (req, res) => {
-    res.setHeader("Content-Type", "text/html")
+    res.header("Content-Type", "text/html")
+    // res.header("Access-Control-Allow-Origin", "*")
+
     res.send(`
     <h1>Mock API</h1>
     `)
